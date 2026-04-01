6b0fcf968298a0ed92284360caa2d877ab59a699
Student <ilikecats2250@gmail.com>
2026-04-01 15:18:31 +0300

    Test

--- src/main/java/loose/oose/fis/lab/student/manager/model/Student.java
+++ src/main/java/loose/oose/fis/lab/student/manager/model/Student.java
@@ -1,28 +1,44 @@
 package loose.oose.fis.lab.student.manager.model;
 
+import java.util.Objects;
+
 public class Student {
     private String firstName;
     private String lastName;
     private int age;
     private double meanGrade;
 
-    public Student(String firstName, String lastName, int age, double meanGrade) {
-        //assignments
+
+    public Student() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     @Override
     public String toString() {
-        //toString generated implementation
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", meanGrade=" + meanGrade +
+                '}';
     }
 
     @Override
     public boolean equals(Object o) {
-        //equals generated implementation
+        if (!(o instanceof Student student)) return false;
+        return age == student.age && Double.compare(meanGrade, student.meanGrade) == 0 && Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName);
     }
 
     @Override
     public int hashCode() {
-        //hashCode generated implementation
+        return Objects.hash(firstName, lastName, age, meanGrade);
     }
 
     //generated getters and setters for all fields
