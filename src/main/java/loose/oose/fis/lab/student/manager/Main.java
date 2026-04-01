175687588215dba23dfa4d5106cf8e972da8c86c
FaZeCristy <96000066+FaZeCristy@users.noreply.github.com>
2026-04-01 15:33:30 +0300

    Apply suggestion from @FaZeCristy

--- src/main/java/loose/oose/fis/lab/student/manager/Main.java
+++ src/main/java/loose/oose/fis/lab/student/manager/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader root = FXMLLoader.load(getClass().getResource("fxml/view-students.fxml"));
-        primaryStage.setTitle("Student Manager");
+        primaryStage.setTitle("FIS - Student Manager");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
